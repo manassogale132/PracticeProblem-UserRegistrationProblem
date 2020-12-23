a5378f7e70f09c366b6974280b8f67cb78104c25
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2020-12-23 17:07:44 +0530

    Added : FirstName Test Case

--- src/test/java/UserValidatorTestCases.java
+++ src/test/java/UserValidatorTestCases.java
@@ -15,54 +15,6 @@ public class UserValidatorTestCases{
         Assert.assertFalse(result);
     }
 
-    @Test                                                                                                                   //  Right Last Name
-    public void lastName_EnteredProper(){
-        boolean result = UserValidator.userValidation("Sogale",UserValidator.lastNamePattern);
-        Assert.assertTrue(result);
-    }
+}  
 
-    @Test
-    public void lastName_EnteredWrong(){                                                                                      // Wrong Last Name
-        boolean result = UserValidator.userValidation("sogale",UserValidator.lastNamePattern);
-        Assert.assertFalse(result);
-    }
-
-    @Test                                                                                                                     // Right Email
-    public void email_EnteredProper(){
-        boolean result = UserValidator.userValidation("jasonSmith@gmail.com",UserValidator.emailIdPattern);
-        Assert.assertTrue(result);
-    }
-
-    @Test                                                                                                                     // Wrong Email
-    public void email_EnteredWrong(){
-        boolean result = UserValidator.userValidation("jasonSmith",UserValidator.emailIdPattern);
-        Assert.assertFalse(result);
-    }
-
-    @Test                                                                                                                     // Right PhoneNumber
-    public void phonenumber_EnteredProper(){
-        boolean result = UserValidator.userValidation("91 9632145632",UserValidator.phoneNumberPattern);
-        Assert.assertTrue(result);
-    }
-
-    @Test                                                                                                                     // Wrong PhoneNumber
-    public void phonenumber_EnteredWrong(){
-        boolean result = UserValidator.userValidation("91321456325",UserValidator.phoneNumberPattern);
-        Assert.assertFalse(result);
-    }
-
-    @Test                                                                                                                     // Right Password
-    public void password_EnteredProper(){
-        boolean result = UserValidator.userValidation("nighTRid@13",UserValidator.passwordPattern);
-        Assert.assertTrue(result);
-    }
-
-    @Test                                                                                                                     // Wrong Password
-    public void password_EnteredWrong(){
-        boolean result = UserValidator.userValidation("nightRig",UserValidator.passwordPattern);
-        Assert.assertFalse(result);
-    }
-
-
-
-}
+ 
